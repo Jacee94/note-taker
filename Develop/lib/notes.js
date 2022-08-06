ddf5c21677398ad4d227b94b857c4ae0348d6320
jace.e94 <jace.edwards94@gmail.com>
2022-08-05 23:57:23 -0600

    create new note and validate note functionality

--- Develop/lib/notes.js
+++ Develop/lib/notes.js
@@ -1,10 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
-function validateNote(){
-    
+function validateNote(note){
+    if (!note.title || typeof note.title !== 'string'){
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string'){
+        return false;
+    }
+    if (!note.id || typeof note.id !== 'string'){
+        return false;
+    }
+    return true;
 }
 
-function createNewNote(){
+function createNewNote(body, noteArray){
+    const note = body;
+    noteArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify({ notes: noteArray}, null, 2)
+    );
+    return note;
+}
 
+module.exports = {
+    validateNote,
+    createNewNote
 }
--- Develop/routes/apiRoutes/notesRoutes.js
+++ Develop/routes/apiRoutes/notesRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db');
 const { v4: uuidv4 } = require('uuid');
+const { validateNote, createNewNote } = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     results = notes;
@@ -14,7 +15,7 @@ router.post('/notes', (req, res) => {
         res.status(400).send('The note is not formatted properly');
     } else {
         const note = createNewNote(req.body, notes);
-        res.json(notes);
+        res.json(note);
     }
 });
 
